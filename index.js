345ee02d0fc03b380cdc526151cc764d85addc44
Jessica Greene <jessica0greene@gmail.com>
2017-09-24 15:40:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,10 +24,14 @@ function viewCart() {
   } else if(cart.length === 2) {
       console.log("In your cart, you have " + Object.keys(cart[0]) + " at $" + cart[0][Object.keys(cart[0])] + " and " + Object.keys(cart[1]) + " at $" + cart[1][Object.keys(cart[1])] +  ".")
   } else {
+    var cartList = [];
+    var a = parseInt(cart.length-1)
     for (let i = 0; i < cart.length-1; i++){
-      cartList.push(" " + Object.keys(cart[i]) + " at $" + cart[i][Object.keys(cart[i])] + "," )
-    console.log("In your cart, you have " +  + " and " + Object.keys(cart[i]) + " at $" + cart[i][Object.keys(cart[i])] + ".")
-    }
+      cartList.push(Object.keys(cart[i]) + " at $" + cart[i][Object.keys(cart[i])])
+    }  
+    //console.log(cartList)
+    console.log("In your cart, you have " + String(cartList) + " and " + Object.keys(cart[a]) + " at $" + cart[a][Object.keys(cart[a])] + ".")
+ 
   }
 }
 
