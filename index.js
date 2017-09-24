146eb6bc1c1743b1e88a08b02db4150be20cfcc9
Jessica Greene <jessica0greene@gmail.com>
2017-09-24 15:42:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for ( let i = 0; i < cart.length; i++) {
+  for ( let i = 0; i < cart.length-1; i++) {
     if (String(Object.keys(cart[i])) === item) {
       cart.splice(i,1)
       return cart
